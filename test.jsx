c5016496d70cd172e13b38329ea13e4f26eb5e7c
Jayan Kapali <jayan.kapali@auxfin.com>
2023-04-24 09:14:51 +0545

    features: remove decrement and reset

--- test.jsx
+++ test.jsx
@@ -1,31 +1,17 @@
 import {useState} from 'react';
 
 function Counter(){
-    const [counter1, setCounter] = useState(0);
+    const [counter, setCounter] = useState(0);
 
     return (
         <div>
-            <h1>{counter1}</h1>
+            <h1>{counter}</h1>
             <button
                 onClick={()=>{
                     setCounter(i => (i+1))
                 }}
             >
                 Increment
-            </button>
-            <button
-                onClick={()=>{
-                    setCounter(i => (i-1))
-                }}
-            >
-                Decrement
-            </button>
-            <button
-                onClick={()=>{
-                    setCounter(0)
-                }}
-            >
-                Reset
             </button>
         </div>
     )
