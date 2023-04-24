abef6794b89f3bd7cae159d95a4c38a7bd9bbf4b
Jayan Kapali <jayan.kapali@auxfin.com>
2023-04-24 08:59:14 +0545

    features: add decrement and reset logic

--- test.jsx
+++ test.jsx
@@ -7,9 +7,25 @@ function Counter(){
         <div>
             <h1>{counter}</h1>
             <button
-                onClick={()=>{setCounter(i => (i+1))}}
+                onClick={()=>{
+                    setCounter(i => (i+1))
+                }}
             >
                 Increment
+            </button>
+            <button
+                onClick={()=>{
+                    setCounter(i => (i-1))
+                }}
+            >
+                Decrement
+            </button>
+            <button
+                onClick={()=>{
+                    setCounter(0)
+                }}
+            >
+                Reset
             </button>
         </div>
     )
