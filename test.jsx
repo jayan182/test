f24fb51a08d4706be87a3f76fc2b3bb92b1742aa
Jayan Kapali <jayan.kapali@auxfin.com>
2023-05-01 09:25:36 +0545

    features: change text

--- test.jsx
+++ test.jsx
@@ -11,7 +11,7 @@ function Counter(){
                     setCounter(i => (i+1))
                 }}
             >
-                Increment
+                Increase
             </button>
         </div>
     )
